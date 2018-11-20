d9805a8c3cca7476b235087a44a40c21a4dcc92a
Nick Walker <nickwalker037@gmail.com>
2018-11-20 14:11:57 -0500

    Update OrthogonalMatchingPursuit.m

--- OrthogonalMatchingPursuit.m
+++ OrthogonalMatchingPursuit.m
@@ -8,6 +8,9 @@ b = [1.1862; -0.1158; -0.1093];
 
 xval = [];
 
+% -------------------------------------------------------------
+% ----------------- Version 1 ---------------------------------
+% -------------------------------------------------------------
 
 % This creates arrays of the residual value for different values of our support vector x
 % We search for the best solution with a support of only 1 atom
@@ -28,6 +31,60 @@ end
 
 
 %x = [0;0.915;0;0.719];
+
+
+
+
+
+
+
+% -------------------------------------------------------------
+% ----------------- Version 2 ---------------------------------
+% -------------------------------------------------------------
+
+
+
+resid = norm((A*x)-b,2);
+while resid > k
+  col_res_values = [];
+  col_x_values = [];
+  for j = 1:size(A,2)
+    if x(j) == 0
+      res = [];
+      xvals = [];
+      for i = -3.0:0.01:3.0
+        x(j) = i;
+        res_temp = norm((A*x)-b,2); 
+        res = [res, res_temp]; 
+        xvals = [xvals, i]; 
+      end
+      min_res = min(res);
+      min_x = xvals(find(res == min_res));
+      x(j)=0;
+    else
+      min_res = 100;
+      min_x = 100;
+    end
+  col_res_values = [col_res_values, min_res]; 
+  col_x_values = [col_x_values, min_x];
+  end
+col_index = find(col_res_values == min(abs(col_res_values)));
+x(col_index) = col_x_values(col_index);
+resid = abs(norm((A*x)-b,2));
+s=nnz(sparse(x));
+end
+y = x;
+
+
+
+
+
+
+
+
+
+
+
 
 
 
